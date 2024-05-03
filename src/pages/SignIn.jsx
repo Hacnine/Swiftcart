a42299192ceabd4ac942b79abcff52e5ae2043fe
hacnine <hacninetusar@gmail.com>
2024-05-03 14:31:00 +0600

    bug

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,15 +2,7 @@
 import {SignIn} from "../components/signinandup/index"
 
 const SignInPage = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
-
+  
   return (
     <>
     <SignIn/>
